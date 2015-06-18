87a4933b35dbf72419288e3ec886763511c1ee75
Apon Palanuwech <reach.apon@gmail.com>
2015-06-18 11:37:00 -0400

    - add webserver

---
+++ publicSite/server.js
@@ -0,0 +1,22 @@
+var express = require("express"),
+    app = express(),
+    bodyParser = require('body-parser'),
+    errorHandler = require('errorhandler'),
+    methodOverride = require('method-override'),
+    hostname = process.env.HOSTNAME || 'localhost',
+    port = parseInt(process.env.PORT, 10) || 8000,
+    publicDir = process.argv[2] || __dirname + '/app';
+
+app.use(methodOverride());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static(publicDir));
+app.use(errorHandler({
+  dumpExceptions: true,
+  showStack: true
+}));
+
+console.log("Simple static server showing %s listening at http://%s:%s", publicDir, hostname, port);
+app.listen(port);
