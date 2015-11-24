1c86e89b038484ab468981702d78bdb63038a519
jmsaavedra <mailbox.jms@gmail.com>
2015-11-24 17:01:48 +0100

    latest on-site

--- cameraApp/app.js
+++ cameraApp/app.js
@@ -139,18 +139,18 @@ function snap(){
     console.log(chalk.red('ERROR takePhotos:'),e);
     //rimraf(path.join(global.RAW_IMG_FOLDER, '*.jpg'), function(_e){
     //  if(_e) console.log('error deleting worthless images: '+_e);
-    if (takeNumber > 2){
+    if (takeNumber > 3){
       Mailer.sendEmail('[elBulli cameraApp] Failing on Snap', 'cameras.takePhotos error: \n\n[ '+e+' ]\n\n... restarting app now', function(er){
         if(er) console.log(chalk.red('error sending nodemail: '),er);
-        console.log('>>> QUITTING APP in 10 secs...');
+        console.log('>>> QUITTING APP in 30 secs...');
         setTimeout(function(){
           process.exit(0);
-        },10000);
+        },120000);
       });
     } else{
-      console.log('trying SNAP() again in 10 secs...');
-      io.sockets.emit('error', e+'\n\nTrying snap again in 10 seconds...');
-      setTimeout(function(){snap();},10000);  
+      console.log('trying SNAP() again in 30 secs...');
+      io.sockets.emit('error', e+'\n\nTrying snap again in 30 seconds...');
+      setTimeout(function(){snap();},30000);  
     }
     //}); 
     return false;
